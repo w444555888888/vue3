c74d995d54d35d5a7be00b2ff0f14095bf8fb41b
Mike <w444555888@yahoo.com.tw>
2024-04-17 17:10:13 +0800

    [update]login頁面+路由配置
    [update]login頁面+路由配置

---
+++ src/components/Login.vue
@@ -0,0 +1,144 @@
+<!-- Login.vue -->
+<template>
+    <div>
+        <div id="title">LogIn</div>
+        <form @submit.prevent="login">
+            <div class="input-group">
+                <div class="input-row">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" v-model="username">
+                </div>
+                <div class="input-row">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" v-model="password">
+                </div>
+            </div>
+            <div class="button-container">
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios'
+import { useRouter } from 'vue-router';
+import { ElNotification } from 'element-plus'
+
+const username = ref('');
+const password = ref('');
+const router = useRouter();
+
+const login = () => {
+    axios.get('http://localhost:3000/users')
+        .then(response => {
+            if (response.status === 200) {
+                const dataUsers = response.data;
+                const currentUser = dataUsers.find(user => user.username === username.value && user.password === password.value);
+                if (currentUser) {
+                    ElNotification({
+                        title: 'Success',
+                        message: 'successfully',
+                        type: 'success',
+                    });
+                    localStorage.setItem('token', JSON.stringify(currentUser));
+                    router.push('/');
+                } else {
+                    ElNotification({
+                        title: 'Error',
+                        message: 'Invalid username or password',
+                        type: 'error',
+                    });
+                }
+            }
+        })
+        .catch(error => {
+            ElNotification({
+                title: 'Error',
+                message: 'response Failed ',
+                type: 'error',
+            });
+        });
+};
+
+
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif
+}
+
+#title {
+    font-size: 30px;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.input-group {
+    display: flex;
+    flex-direction: column;
+}
+
+.input-row {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px; 
+}
+
+label {
+    width: 100px;
+    height: 60px;
+    border-radius: 0.2rem;
+    background-color: #dddddd;
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 60px;
+}
+
+input {
+    font-size: 1.125rem;
+    padding: 1rem 2rem;
+    background-color: #fff;
+    border-radius: 0.2rem;
+    width: 500px;
+    word-wrap: break-word;
+    outline: none;
+}
+
+input:focus,
+input:valid {
+    color: black;
+    border: 4px solid #9f9f9f;
+}
+
+input:not(:focus) {
+    border: 4px solid #e3e2e2;
+}
+
+.button-container {
+    display: flex;
+    justify-content: center;
+}
+
+button {
+    cursor: pointer;
+    border: 1px solid #a0a4d9;
+    color: #1f2023;
+    font-size: 16px;
+    font-weight: bold;
+    outline: none;
+    border-radius: 0.5rem;
+    background-color: #7e7e7e;
+    width: 100px;
+    height: 40px;
+    padding: 5px;
+    margin-top: 10px;
+}
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,7 @@ import TodoDetail from '../components/TodoDetail.vue'
 import Cropper from '../components/Cropper.vue'
 import Child from '../components/child.vue'
 import Child2 from '../components/child2.vue'
+import Login from '../components/Login.vue'
 
 
 const routes = [
@@ -28,7 +29,7 @@ const routes = [
                     B: Child2,
 
                 }
-               
+
             }
         ]
     },
@@ -37,6 +38,11 @@ const routes = [
         name: 'Cropper',
         component: Cropper,
     },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    }
 ]
 
 
@@ -45,5 +51,19 @@ const router = createRouter({
     history: createWebHashHistory(),
     routes,
 })
+
+
+router.beforeEach((to, from, next) => {
+    const authenticate = localStorage.getItem('token'); // 检查本地存储是否有 token
+
+    if (!authenticate && to.name !== 'Login') {
+        next('/login');
+    } else if (authenticate && to.name === 'Login') {
+        next('/');
+    } else {
+        next();
+    }
+
+});
 
 export default router
