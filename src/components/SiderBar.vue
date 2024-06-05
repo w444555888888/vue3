0ccf82adee1b854434af74878f236dd70722c2a2
Mike <w444555888@yahoo.com.tw>
2024-06-05 18:04:46 +0800

    [update]siderBar.vue  做一半suspense
    [update]siderBar.vue  做一半suspense

--- src/components/SiderBar.vue
+++ src/components/SiderBar.vue
@@ -1,8 +1,10 @@
 <!-- SiderBar -->
 <template>
     <div class="common-layout">
+
         <el-container>
-            <el-aside width="300px"> <el-scrollbar>
+            <el-aside width="300px">
+                <el-scrollbar>
                     <el-menu :default-openeds="['1']">
                         <el-sub-menu index="1">
                             <template #title>
@@ -14,45 +16,82 @@
                             </template>
 
                             <el-menu-item-group>
-                                <el-menu-item index="1-1"><router-link to="/" class="visited">{{
-                                    t("home") }}</router-link>
+                                <el-menu-item index="1-1">
+                                    <router-link to="/" class="visited">{{ t("home") }}</router-link>
                                 </el-menu-item>
-                                <el-menu-item index="1-2"><router-link to="/todoList" class="visited">{{
-                                    t("titleFirst") }}</router-link>
+                                <el-menu-item index="1-2">
+                                    <router-link to="/todoList" class="visited">{{ t("titleFirst") }}</router-link>
                                 </el-menu-item>
-                                <el-menu-item index="1-3"><router-link to="/personalize" class="visited">{{
-                                    t("personalize") }}</router-link></el-menu-item>
+                                <el-menu-item index="1-3">
+                                    <router-link to="/personalize" class="visited">{{ t("personalize") }}</router-link>
+                                </el-menu-item>
                                 <el-menu-item index="1-4">Option 1-4</el-menu-item>
                             </el-menu-item-group>
                         </el-sub-menu>
                     </el-menu>
-                </el-scrollbar></el-aside>
+                </el-scrollbar>
+            </el-aside>
             <el-main>
-                <router-view :key="globalStore.routeKey"></router-view>
+                <template v-if="currentRoute && currentRoute.matched.length > 0 && currentRoute.matched[0].components">
+
+                    <KeepAlive>
+                        <Suspense>
+                            <!-- 主要内容 -->
+                            <component :is="currentRoute.matched[0].components.default"></component>
+
+
+                            <template #fallback>
+                                <div class="loading">
+                                    <el-loading :visible="isLoading" :lock="true" text="Loading"
+                                        background="rgba(0, 0, 0, 0.7)">
+                                       
+                                    </el-loading>
+                                </div>
+                            </template>
+                        </Suspense>
+                    </KeepAlive>
+
+                </template>
             </el-main>
         </el-container>
+
     </div>
 </template>
 
 
 <script setup>
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch, onMounted ,Suspense,KeepAlive } from 'vue'
 import { usePiniaStore } from '../store/pinia'
 import i18n from '../i18n.js'
 import { updateUserImage } from './compoent-items/UpdateUserImage.js'
-import { useGlobalStore } from '../store/globalStore'
+import { useRouter } from 'vue-router';
+import { ElLoading } from 'element-plus';
+const router = useRouter();
+const currentRoute = router.currentRoute;
+// loading
+const isLoading = ref(true);
 
-const globalStore = useGlobalStore()
+watch(currentRoute, (to, from) => {
+    console.log('Route changed from', from, 'to', to);
+    isLoading.value = true;
+});
 
-const onClick = () => {
-  globalStore.routeKey++
-}
+// 在路由导航开始之前设置isLoading为true
+router.beforeEach((to, from, next) => {
+  isLoading.value = true;
+  next();
+});
+
+// 在路由导航结束之后设置isLoading为false
+router.afterEach((to, from) => {
+  isLoading.value = true;
+});
+
 
 const { t } = i18n.global
 const store = usePiniaStore()
+
 const imgValue = ref(updateUserImage())
-
-
 onMounted(() => {
     updateUserImage()
 })
@@ -106,10 +145,18 @@ $secondTextColor: #1f2023;
     }
 }
 
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px; 
+  background-color: blue;
+}
+
+
 :deep(.el-sub-menu__title) {
     height: 100px;
 }
-
 
 // 移除預設超連結樣式
 .visited {
